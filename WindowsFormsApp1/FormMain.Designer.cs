b0bf771b7598c93c284ac6ebb7111e34017abf7c
Tyuiu.AleevRI@outlook.com <aleevruslan3@gmail.com>
2024-03-02 17:28:22 +0500

    WinForm

--- WindowsFormsApp1/FormMain.Designer.cs
+++ WindowsFormsApp1/FormMain.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -30,15 +30,15 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox_Materials = new System.Windows.Forms.ComboBox();
-            this.textBox_size_shirina = new System.Windows.Forms.TextBox();
-            this.textBox_size_dlina = new System.Windows.Forms.TextBox();
-            this.textBox_size_visota = new System.Windows.Forms.TextBox();
-            this.label_Input = new System.Windows.Forms.Label();
-            this.label_Materials = new System.Windows.Forms.Label();
-            this.textBox_Output = new System.Windows.Forms.TextBox();
-            this.label_Output = new System.Windows.Forms.Label();
             this.button_Calculate = new System.Windows.Forms.Button();
+            this.label_Output = new System.Windows.Forms.Label();
+            this.textBox_Output = new System.Windows.Forms.TextBox();
+            this.label_Materials = new System.Windows.Forms.Label();
+            this.label_Input = new System.Windows.Forms.Label();
+            this.textBox_size_visota = new System.Windows.Forms.TextBox();
+            this.textBox_size_dlina = new System.Windows.Forms.TextBox();
+            this.textBox_size_shirina = new System.Windows.Forms.TextBox();
+            this.comboBox_Materials = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,61 +62,14 @@ namespace WindowsFormsApp1
             this.groupBox1.TabStop = false;
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // comboBox_Materials
+            // button_Calculate
             // 
-            this.comboBox_Materials.Items.AddRange(new object[] {
-            "Кирпич",
-            "Шлакоблок"});
-            this.comboBox_Materials.Location = new System.Drawing.Point(24, 56);
-            this.comboBox_Materials.Name = "comboBox_Materials";
-            this.comboBox_Materials.Size = new System.Drawing.Size(119, 21);
-            this.comboBox_Materials.TabIndex = 1;
-            // 
-            // textBox_size_shirina
-            // 
-            this.textBox_size_shirina.Location = new System.Drawing.Point(24, 187);
-            this.textBox_size_shirina.Name = "textBox_size_shirina";
-            this.textBox_size_shirina.Size = new System.Drawing.Size(101, 20);
-            this.textBox_size_shirina.TabIndex = 2;
-            // 
-            // textBox_size_dlina
-            // 
-            this.textBox_size_dlina.Location = new System.Drawing.Point(131, 187);
-            this.textBox_size_dlina.Name = "textBox_size_dlina";
-            this.textBox_size_dlina.Size = new System.Drawing.Size(100, 20);
-            this.textBox_size_dlina.TabIndex = 3;
-            // 
-            // textBox_size_visota
-            // 
-            this.textBox_size_visota.Location = new System.Drawing.Point(238, 187);
-            this.textBox_size_visota.Name = "textBox_size_visota";
-            this.textBox_size_visota.Size = new System.Drawing.Size(100, 20);
-            this.textBox_size_visota.TabIndex = 4;
-            // 
-            // label_Input
-            // 
-            this.label_Input.AutoSize = true;
-            this.label_Input.Location = new System.Drawing.Point(22, 161);
-            this.label_Input.Name = "label_Input";
-            this.label_Input.Size = new System.Drawing.Size(148, 13);
-            this.label_Input.TabIndex = 5;
-            this.label_Input.Text = "Габариты материала (ШДВ)";
-            // 
-            // label_Materials
-            // 
-            this.label_Materials.AutoSize = true;
-            this.label_Materials.Location = new System.Drawing.Point(24, 37);
-            this.label_Materials.Name = "label_Materials";
-            this.label_Materials.Size = new System.Drawing.Size(98, 13);
-            this.label_Materials.TabIndex = 6;
-            this.label_Materials.Text = "Выбор материала";
-            // 
-            // textBox_Output
-            // 
-            this.textBox_Output.Location = new System.Drawing.Point(25, 386);
-            this.textBox_Output.Name = "textBox_Output";
-            this.textBox_Output.Size = new System.Drawing.Size(100, 20);
-            this.textBox_Output.TabIndex = 7;
+            this.button_Calculate.Location = new System.Drawing.Point(24, 235);
+            this.button_Calculate.Name = "button_Calculate";
+            this.button_Calculate.Size = new System.Drawing.Size(119, 42);
+            this.button_Calculate.TabIndex = 9;
+            this.button_Calculate.Text = "Расчитать кол-во материала";
+            this.button_Calculate.UseVisualStyleBackColor = true;
             // 
             // label_Output
             // 
@@ -127,16 +80,63 @@ namespace WindowsFormsApp1
             this.label_Output.TabIndex = 8;
             this.label_Output.Text = "Вывод";
             // 
-            // button_Calculate
+            // textBox_Output
             // 
-            this.button_Calculate.Location = new System.Drawing.Point(24, 235);
-            this.button_Calculate.Name = "button_Calculate";
-            this.button_Calculate.Size = new System.Drawing.Size(119, 42);
-            this.button_Calculate.TabIndex = 9;
-            this.button_Calculate.Text = "Расчитать кол-во материала";
-            this.button_Calculate.UseVisualStyleBackColor = true;
+            this.textBox_Output.Location = new System.Drawing.Point(25, 386);
+            this.textBox_Output.Name = "textBox_Output";
+            this.textBox_Output.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Output.TabIndex = 7;
             // 
-            // Form1
+            // label_Materials
+            // 
+            this.label_Materials.AutoSize = true;
+            this.label_Materials.Location = new System.Drawing.Point(24, 37);
+            this.label_Materials.Name = "label_Materials";
+            this.label_Materials.Size = new System.Drawing.Size(98, 13);
+            this.label_Materials.TabIndex = 6;
+            this.label_Materials.Text = "Выбор материала";
+            // 
+            // label_Input
+            // 
+            this.label_Input.AutoSize = true;
+            this.label_Input.Location = new System.Drawing.Point(22, 161);
+            this.label_Input.Name = "label_Input";
+            this.label_Input.Size = new System.Drawing.Size(148, 13);
+            this.label_Input.TabIndex = 5;
+            this.label_Input.Text = "Габариты материала (ШДВ)";
+            // 
+            // textBox_size_visota
+            // 
+            this.textBox_size_visota.Location = new System.Drawing.Point(238, 187);
+            this.textBox_size_visota.Name = "textBox_size_visota";
+            this.textBox_size_visota.Size = new System.Drawing.Size(100, 20);
+            this.textBox_size_visota.TabIndex = 4;
+            // 
+            // textBox_size_dlina
+            // 
+            this.textBox_size_dlina.Location = new System.Drawing.Point(131, 187);
+            this.textBox_size_dlina.Name = "textBox_size_dlina";
+            this.textBox_size_dlina.Size = new System.Drawing.Size(100, 20);
+            this.textBox_size_dlina.TabIndex = 3;
+            // 
+            // textBox_size_shirina
+            // 
+            this.textBox_size_shirina.Location = new System.Drawing.Point(24, 187);
+            this.textBox_size_shirina.Name = "textBox_size_shirina";
+            this.textBox_size_shirina.Size = new System.Drawing.Size(101, 20);
+            this.textBox_size_shirina.TabIndex = 2;
+            // 
+            // comboBox_Materials
+            // 
+            this.comboBox_Materials.Items.AddRange(new object[] {
+            "Кирпич",
+            "Шлакоблок"});
+            this.comboBox_Materials.Location = new System.Drawing.Point(24, 56);
+            this.comboBox_Materials.Name = "comboBox_Materials";
+            this.comboBox_Materials.Size = new System.Drawing.Size(119, 21);
+            this.comboBox_Materials.TabIndex = 1;
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -146,7 +146,7 @@ namespace WindowsFormsApp1
             this.MaximumSize = new System.Drawing.Size(713, 544);
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(713, 544);
-            this.Name = "Form1";
+            this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Расчет строительных материалов";
             this.groupBox1.ResumeLayout(false);
